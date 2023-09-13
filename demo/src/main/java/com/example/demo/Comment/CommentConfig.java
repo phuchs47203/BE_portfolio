737964fcabc6ed969e23ad0f476306baf3fe593f
phuchs47203 <21522479@gm.uit.edu.vn>
2023-09-13 09:55:48 +0700

    5

--- demo/src/main/java/com/example/demo/Comment/CommentConfig.java
+++ demo/src/main/java/com/example/demo/Comment/CommentConfig.java
@@ -11,16 +11,16 @@ public class CommentConfig {
     @Bean
     CommandLineRunner commandLineRunner(CommentRepository commentRepository) {
         return args -> {
-            CommentUser hoanglong = new CommentUser(
-                    "long123@gmail.com",
-                    "Hoang Long",
-                    "Porfolio",
-                    "very good");
-            CommentUser minhanh = new CommentUser(
-                    "ma@gmail.com",
-                    "minh anh",
-                    "Porfolio",
-                    "very good");
+            // CommentUser hoanglong = new CommentUser(
+            // "long123@gmail.com",
+            // "Hoang Long",
+            // "Porfolio",
+            // "very good");
+            // CommentUser minhanh = new CommentUser(
+            // "ma@gmail.com",
+            // "minh anh",
+            // "Porfolio",
+            // "very good");
 
             // commentRepository.saveAll(List.of(hoanglong, minhanh));
         };
